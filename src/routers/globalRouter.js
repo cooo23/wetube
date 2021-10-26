42b65e4b682318a24c8f07f6aa6aaeeee1fe4f98
cooo23 <85227527+cooo23@users.noreply.github.com>
2021-10-26 23:16:52 +0900

    backend

--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,13 +1,12 @@
 import express from "express";
 import { join, login, logout, see } from "../controllers/userController";
-import { trending, search }from "../controllers/videoController";
+import { trending }from "../controllers/videoController";
 
 const globalRouter = express.Router();
 
 globalRouter.get("/", trending);
 globalRouter.get("/join", join);
 globalRouter.get("/login", login);
-globalRouter.get("/search", search);
 globalRouter.get("/logout", logout);
 globalRouter.get("/see", see);
 
--- src/server.js
+++ src/server.js
@@ -5,13 +5,13 @@ import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 
 const PORT = 4000;
-
 const app = express();
 const logger = morgan("dev");
 
 app.set("view engine","pug")
 app.set("views", process.cwd() + "/src/views");
 app.use(logger);
+app.use(express.urlencoded({ extended: true }));
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
